a2ed9e94071430013e8b93e5d1cb91c2264437ed
amittad <tadamit2004@gmail.com>
2025-10-30 01:30:07 +0530

    change code

--- phase 1/pattern/pract4.py
+++ phase 1/pattern/pract4.py
@@ -1,4 +1,7 @@
 #Print a Right-Aligned Triangle of Stars
-n = int(input("Enter n: "))   # e.g. 5
-for i in range(1, n+1):
-    print(" # " * (n-i)+"*" * i)
+n=int(input("enter a number"))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ", end="")
+    print("*" * i)
+
