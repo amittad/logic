a20172f7b4fa6b1e01a180c931556b5fa9f3089e
amittad <tadamit2004@gmail.com>
2025-11-01 18:51:31 +0530

    add

---
+++ phase 1/pattern/pract9.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+    
+    for j in  range(i):
+        print(" "*(10-j),end="")
+        for k in range(j):
+            print("*" ,end="")
+        
+    print(" ")    
+
+        
+    
