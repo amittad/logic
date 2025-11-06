11430d5ca83d56d4eaf428f97bfab1a4c50651de
amittad <tadamit2004@gmail.com>
2025-11-07 00:42:50 +0530

    list comprehensivet

---
+++ phase 1/array_advance/arr1.py
@@ -0,0 +1,15 @@
+#. Input an element x — check if it exists in the array. 
+arr=[1,2,3,4,5,8,9,34,56]
+num=int(input("enter a number for check this number is exist or not="))
+
+for i in arr:
+    if num == i:
+        a=False
+        
+    else:
+        a=True
+
+print(a)        
+         
+        
+    
---
+++ phase 1/array_advance/arr3.py
@@ -0,0 +1,9 @@
+arr=[1,2,3,4,5,6,7]
+
+num=int(input("Check if the number exists or not: "))
+
+for i in arr:
+    if num==1:
+        print("Number Exists")
+    else:
+        print("NOt exists")
