1a07344d912a04e4c45d84b4643a15ca4737d12c
amittad <tadamit2004@gmail.com>
2025-11-07 22:31:50 +0530

    list comprehensivet

--- phase 1/array_advance/arr4.py
+++ phase 1/array_advance/arr4.py
@@ -1,8 +1,9 @@
 #Find the first occurrence of a given number. 
-arr=[1,2,3,5,6,8,9,3]
-num=3
+arr=[1,2,3,5,6,8,9,8,3]
+num=8
 
 for i in arr:
     if i==num:
-       a= arr.index(i)
-print(a)
+      print(arr.index(i))
+    
+
