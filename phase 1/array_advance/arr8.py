1fdf213afe98df30912fa425804c6a04affa38df
amittad <tadamit2004@gmail.com>
2025-11-08 17:28:56 +0530

    list comprehensivet

--- phase 1/array_advance/arr8.py
+++ phase 1/array_advance/arr8.py
@@ -2,7 +2,7 @@ arr = [10, 2, 3, 4, 51, 6, 33, 1, 22, 67]
 prime_count = 0
 
 for i in arr:
-    if i > 1:  # prime numbers are greater than 1
+    if 1<i:  # prime numbers are greater than 1
         for j in range(2, int(i**0.5) + 1):
             if i % j == 0:
                 break
