fda6199dfb49f4cb1115bd5fd41242392f6a5796
amittad <tadamit2004@gmail.com>
2025-11-10 23:12:16 +0530

    list comprehensivet

---
+++ phase 1/crtical logic/pract1.py
@@ -0,0 +1,9 @@
+#2. Take three numbers and check if they can form a Pythagorean tripleprint
+print("enter a three numbers")
+a=int(input())
+b=int(input())
+c=int(input())
+if(c*c)==(a*a)+(b*b):
+    print("this is triplate")
+else:
+    print("this is not triplate")
