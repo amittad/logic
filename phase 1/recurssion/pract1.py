c41d114e5ccb22f1c1fffaa5ce61032acbbbe7f0
amittad <tadamit2004@gmail.com>
2025-11-27 11:00:36 +0530

    remove class method and addd in object to thec class method

--- phase 1/recurssion/pract1.py
+++ phase 1/recurssion/pract1.py
@@ -1,8 +1,8 @@
 #1 . Print numbers from 1 to n using recursion. 
 def countdown(n):
-    if n<=0:
-        print("done")
-    else:
-        print(n)
-        countdown(n-1)    
+    if n==0:
+        return 
+    
+    countdown(n-1)  
+    print(n)  
 countdown(5)        
---
+++ phase 1/string/pract3.py
@@ -0,0 +1,10 @@
+#negative indexing
+
+a= "amit tad"
+a=a[::1]
+count=0
+for i in a:
+ count+=1
+ print(f"{[count]}=={i}")
+
+print(count)
